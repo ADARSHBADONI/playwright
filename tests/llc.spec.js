86028dd0a7f5877c0f9c9ba9b51cd8c3bed380d8
Adarsh Badoni <adarshbadoni109@gmail.com>
2025-06-07 12:27:12 +0530

    Add login test cases and update README

---
+++ tests/llc.spec.js
@@ -0,0 +1,8 @@
+import {test,expect} from '@playwright/test'
+
+test ('test',async ({page})=>{
+    await page.goto("https://rahulshettyacademy.com/angularpractice/");
+    await page.getByLabel("Check me out if you Love IceCreams!").click();
+    await page.getByRole
+
+})
